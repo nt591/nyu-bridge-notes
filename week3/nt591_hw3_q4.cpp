c789ca97c6616cc0933db6787b6a1995579d240a
Nikhil Thomas <nikhil@arceoanalytics.com>
2020-07-28 11:13:58 -0400

    more clever week3 q4
    Signed-off-by: Nikhil Thomas <nikhil@arceoanalytics.com>

--- week3/nt591_hw3_q4.cpp
+++ week3/nt591_hw3_q4.cpp
@@ -11,7 +11,10 @@ using namespace std;
 const int FLOOR_ROUND = 1;
 const int CEILING_ROUND = 2;
 const int ROUND = 3;
-
+const int NEGATIVE_FLOOR_ROUND = -1;
+const int NEGATIVE_CEILING_ROUND = -2;
+const int NEGATIVE_ROUND = -3;
+const int INTEGER_ROUND = 0;
 int main() {
   double input;
   int rounding;
@@ -23,60 +26,75 @@ int main() {
   cout << "3. Round to the nearest whole number" << endl;
   cin >> rounding;
 
-  // first off, if the input is an integer we don't round so lets capture that case
-  if (input == (int) input) {
-    cout << input << endl;
-  } else {
-    switch(rounding) {
-      case FLOOR_ROUND: {
-        // if negative, need to truncate and subtract one
-        if (input < 0) {
-          int rounded = (int) input - 1;
-          cout << rounded << endl;
-        } else {
-          cout << (int) input << endl;
-        }
-        break;
-      }
-      case CEILING_ROUND: {
-        // if input is negative, just truncate (ceiling of -3.4 is -3)
-        // else truncate and add 1
-        if (input < 0) {
-          cout << (int) input << endl;
-        } else {
-          int rounded = (int) input;
-          cout << rounded + 1 << endl;
-        }
-      }
-      case ROUND: {
-         // two booleans
-         // is it negative?
-         // is the decimal 0.5 or higher?
-         // T, T = round down (-4.7 is closer to -5)
-         // T, F = round up (-4.3 is closer to -4)
-         // F, T = round up
-         // F, F = round down
-        int rounded = (int) input;
-        double decimals = input - rounded;
-        if (input < 0) {
-          if (decimals <= -0.5) {
-            cout << rounded - 1 << endl;
-          } else {
-            cout << rounded << endl;
-          }
-        } else {
-          if (decimals >= 0.5) {
-            cout << rounded + 1 << endl;
-          } else {
-            cout << rounded << endl;
-          }
-        }
-        break;
-      }
-      default: 
-        cout << "Invalid input" << endl;
-        break;
+  int castedToInt = (int) input;
+  bool isInteger = input == castedToInt;
+  float decimals = input - castedToInt;
+  
+  /*
+   * TA note: This exists due to Piazza comments about trying to avoid if/statements
+   * since we're potentially allowed to use nested if statements
+   * BUT cannot wrap the switch in an if/else (where if checks if an integer, else does the switching on rounding input)
+   * then I chose to make some adjustments on the input to allow me one top-level switch
+   */
+
+  int roundedWithSign = rounding;
+  if (isInteger) {
+    roundedWithSign = 0;
+  } else if (input < 0) {
+    roundedWithSign *= -1;
+  }
+  
+  // I could choose to make more changes to the rounding method based on decimals but that seems too far
+  // eg
+  // if ROUND and decimals >= 0.5, adjust to ceiling
+  // if ROUND and decimals < -0.5, adjust to floor
+  // if NEGATIVE_ROUND and < -0.5, negative floor
+  // if NEGATIVE round and >= -0.5, negative ceiling 
+ 
+  cout << "Your input was: " << input << " and after rounding it is "; 
+  switch(roundedWithSign) {
+    case INTEGER_ROUND: {
+      cout << input << endl;
+      break;
+    } 
+    case FLOOR_ROUND: {
+      cout << castedToInt << endl;
+      break;
     }
+    case CEILING_ROUND: {
+      cout << castedToInt + 1 << endl;
+      break;
+    }
+    case NEGATIVE_FLOOR_ROUND: {
+      cout << castedToInt - 1 << endl;
+      break;
+    }
+    case NEGATIVE_CEILING_ROUND: {
+      cout << castedToInt << endl;
+      break;
+   }
+    case ROUND: {
+      // if decimal >= 0.5, round up else down
+      // I couldn't avoid this if/else (or at least wasn't clever enough)
+      if (decimals >= 0.5) {
+        cout << castedToInt + 1 << endl;
+      } else {
+        cout << castedToInt << endl;
+      }
+      break;
+    }
+    case NEGATIVE_ROUND: {
+      // if decimal >=- 0.5, same as NEGATIVE CAIL, else NEGATIVE FLOOR
+      if (decimals >= -0.5) {
+        cout << castedToInt << endl;
+      } else {
+        cout << castedToInt - 1<< endl;
+      }
+      break;
+    }
+    default: 
+      cout << "Invalid input" << endl;
+      break;
   }
   
   return 0;
